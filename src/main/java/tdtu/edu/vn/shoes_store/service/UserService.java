1cd7df762048cc0c4a441e87c5693965666fcf52
trinhduy444 <52000655@student.tdtu.edu.vn>
2023-04-27 22:08:38 +0700

    Finish getAll, update, delete User but no status, i will fix it later

--- src/main/java/tdtu/edu/vn/shoes_store/service/UserService.java
+++ src/main/java/tdtu/edu/vn/shoes_store/service/UserService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import tdtu.edu.vn.shoes_store.dto.UserDto;
 import tdtu.edu.vn.shoes_store.model.User;
 
+import java.util.List;
+
 @Service
 public interface UserService {
     void registerUser(UserDto userDto);
@@ -12,4 +14,9 @@ public interface UserService {
     UserDto findUserByID(Long id);
 
     UserDto updateUserByID(Long id,UserDto userDto);
+    List<UserDto> getAllUser();
+
+    UserDto addUser(UserDto userDto);
+
+    boolean deleteUserByID(Long id);
 }
