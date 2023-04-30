d4068eaa480babed92a525d77ebb40e1ba431a64
trinhduy444 <52000655@student.tdtu.edu.vn>
2023-04-30 16:46:24 +0700

    add order in profile id

--- src/main/java/tdtu/edu/vn/shoes_store/controller/UserController.java
+++ src/main/java/tdtu/edu/vn/shoes_store/controller/UserController.java
@@ -5,11 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tdtu.edu.vn.shoes_store.dto.OrderDto;
+import tdtu.edu.vn.shoes_store.dto.ProductOrderDto;
 import tdtu.edu.vn.shoes_store.dto.UserDto;
+import tdtu.edu.vn.shoes_store.model.Order;
+import tdtu.edu.vn.shoes_store.model.OrderDetail;
+import tdtu.edu.vn.shoes_store.model.User;
+import tdtu.edu.vn.shoes_store.service.OrderService;
 import tdtu.edu.vn.shoes_store.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +27,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private OrderService orderService;
     @GetMapping
     public  ResponseEntity<List<UserDto>> getAllUser(){
         List<UserDto> userDtoList = userService.getAllUser();
@@ -47,12 +56,26 @@ public class UserController {
 
     }
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserByID(@PathVariable(name = "id") Long id) {
-        UserDto userDto = userService.findUserByID(id);
-        if (userDto == null) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> getUserByID(@PathVariable(name = "id") Long id) {
+        Map<String, Object> result = new HashMap<>();
+
+        UserDto userDtotmp = userService.findUserByID(id);
+
+        User user = userService.findUserByEmail(userDtotmp.getEmail());
+        if (user != null) {
+            List<Order> orderList = orderService.findOrderByEmail(userDtotmp.getEmail());
+            UserDto userDto = getUserDtoBody(user, orderList);
+            result.put("statusCode", HttpStatus.OK.value());
+            result.put("timeStamp", LocalTime.now());
+            result.put("message", "Get profile successfully!");
+            result.put("content", userDto);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            result.put("statusCode", HttpStatus.NOT_FOUND.value());
+            result.put("timeStamp", LocalTime.now());
+            result.put("message", "User not found!");
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
         }
-        return ResponseEntity.ok(userDto);
     }
 
 //    @PutMapping("/{id}")
@@ -64,6 +87,60 @@ public class UserController {
 //            return ResponseEntity.notFound().build();
 //        }
 //    }
+    private UserDto getUserDtoBody(User user, List<Order> orderList) {
+        UserDto userDto = new UserDto();
+
+        if (user.getId() != null) userDto.setId(user.getId());
+        if (user.getEmail() != null) userDto.setEmail(user.getEmail());
+        if (user.getUsername() != null) userDto.setUsername(user.getUsername());
+        if (user.getGender() != null) userDto.setGender(user.getGender());
+        if (user.getPhone() != null) userDto.setPhone(user.getPhone());
+        if (user.getAddress() != null) userDto.setAddress(user.getAddress());
+        if (user.getAge() != null) userDto.setAge(user.getAge());
+        if (user.getPassword() != null) userDto.setPassword(user.getPassword());
+
+        userDto.setOrders(getListOrder(orderList));
+
+        return userDto;
+    }
+
+    private List<OrderDto> getListOrder(List<Order> orders) {
+        List<OrderDto> listOrder = new ArrayList<>();
+        for (Order order : orders) {
+            OrderDto orderDto = getOrderDtoBody(order);
+            listOrder.add(orderDto);
+        }
+        return listOrder;
+    }
+    private OrderDto getOrderDtoBody(Order order) {
+        OrderDto orderDto = new OrderDto();
+
+        if (order.getId() != null) orderDto.setId(order.getId());
+        if (order.getEmail() != null) orderDto.setEmail(order.getEmail());
+        if (order.getAddress() != null) orderDto.setAddress(order.getAddress());
+        if (order.getDate() != null) orderDto.setDate(order.getDate());
+        if (order.getPayment() != null) orderDto.setPayment(order.getPayment());
+        if (order.getStatus() != null) orderDto.setStatus(order.getStatus());
+        orderDto.setTotalPrice(order.getTotalPrice());
+
+        List<ProductOrderDto> productDtoList = orderDto.getProducts();
+
+        for (OrderDetail orderDetail : order.getOrderDetail()) {
+            ProductOrderDto productOrders = new ProductOrderDto();
+
+            productOrders.setName(orderDetail.getProduct().getName());
+            productOrders.setDescription(orderDetail.getProduct().getDescription());
+            productOrders.setImage(orderDetail.getProduct().getImage());
+            productOrders.setSize(orderDetail.getSize());
+            productOrders.setQuantity(orderDetail.getQuantity());
+            productOrders.setPrice(orderDetail.getProduct().getPrice());
+
+            productDtoList.add(productOrders);
+        }
+
+        orderDto.setProducts(productDtoList);
+        return orderDto;
+    }
 
 
     @PostMapping("")
